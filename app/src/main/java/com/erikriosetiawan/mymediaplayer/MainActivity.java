6cde68fb50f55efaeec05625185244e391ec7a55
erik1997 <erikriosetiawan49@gmail.com>
2019-07-12 19:11:11 +0700

    Adding Services and Notification

--- app/src/main/java/com/erikriosetiawan/mymediaplayer/MainActivity.java
+++ app/src/main/java/com/erikriosetiawan/mymediaplayer/MainActivity.java
@@ -1,21 +1,27 @@
 package com.erikriosetiawan.mymediaplayer;
 
-import android.content.res.AssetFileDescriptor;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnPlay;
     private Button btnStop;
-    private MediaPlayer mMediaPlayer = null;
-    private boolean isReady;
+
+    private final String TAG = MainActivity.class.getSimpleName();
+    private Messenger mService = null;
+    private Intent mBoundServiceIntent;
+    private boolean mServiceBound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,58 +34,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnPlay.setOnClickListener(this);
         btnStop.setOnClickListener(this);
 
-        init();
+        mBoundServiceIntent = new Intent(MainActivity.this, MediaService.class);
+        mBoundServiceIntent.setAction(MediaService.ACTION_CREATE);
+
+        startService(mBoundServiceIntent);
+        bindService(mBoundServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
     }
 
-    private void init() {
-        mMediaPlayer = new MediaPlayer();
-        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-        AssetFileDescriptor afd = getApplicationContext().getResources().openRawResourceFd(R.raw.guitar_background);
-        try {
-            mMediaPlayer.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-        } catch (IOException e) {
-            e.printStackTrace();
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mService = new Messenger(service);
+            mServiceBound = true;
         }
 
-        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                isReady = true;
-                mMediaPlayer.start();
-            }
-        });
-        mMediaPlayer.setOnErrorListener(new MediaPlayer.OnErrorListener() {
-            @Override
-            public boolean onError(MediaPlayer mp, int what, int extra) {
-                return false;
-            }
-        });
-    }
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mService = null;
+            mServiceBound = false;
+        }
+    };
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
         switch (id) {
             case R.id.btn_play:
-                if (!isReady) {
-                    mMediaPlayer.prepareAsync();
-                } else {
-                    if (mMediaPlayer.isPlaying()) {
-                        mMediaPlayer.pause();
-                    } else {
-                        mMediaPlayer.start();
-                    }
+                if (!mServiceBound) return;
+                try {
+                    mService.send(Message.obtain(null, MediaService.PLAY, 0, 0));
+                } catch (RemoteException e) {
+                    e.printStackTrace();
                 }
                 break;
             case R.id.btn_stop:
-                if (mMediaPlayer.isPlaying() || isReady) {
-                    mMediaPlayer.stop();
-                    isReady = false;
+                if (!mServiceBound) return;
+                try {
+                    mService.send(Message.obtain(null, MediaService.STOP, 0, 0));
+                } catch (RemoteException e) {
+                    e.printStackTrace();
                 }
                 break;
             default:
                 break;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+        unbindService(mServiceConnection);
+        mBoundServiceIntent.setAction(MediaService.ACTION_DESTROY);
+        startService(mBoundServiceIntent);
     }
 }
---
+++ app/src/main/java/com/erikriosetiawan/mymediaplayer/MediaPlayerCallback.java
@@ -0,0 +1,6 @@
+package com.erikriosetiawan.mymediaplayer;
+
+public interface MediaPlayerCallback {
+    void onPlay();
+    void onStop();
+}
